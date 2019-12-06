08161e268131d85118be62960adcb28f02adbdff
mavroian <mavflorin@gmail.com>
2019-12-07 01:14:27 +0900

    added BrowserRouter on client

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,26 @@
 //startup point for the side clint application
+import "babel-polyfill"
 import React from "react";
 import ReactDom from "react-dom"
-import Homepage from "./components/homepage/homepage.component";
+import { BrowserRouter } from "react-router-dom"
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import Routes from "./Routes"
+import reducers from "./Reducers"
 
-ReactDom.hydrate(<Homepage />, document.getElementById("root"));
+// create store to use on the client side
+let initialState = {}
+const store = createStore(reducers, initialState, applyMiddleware(thunk));
+
+
+ReactDom.hydrate(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
+
+  ,
+  document.getElementById("root")
+);
