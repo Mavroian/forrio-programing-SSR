53983bbb8fab0842a62ef92338fc12b8c85d7099
mavroian <mavflorin@gmail.com>
2019-12-07 01:13:19 +0900

    Added static router to server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+import "babel-polyfill"
 import express from 'express';
 import React from 'react';
 import { renderToString } from "react-dom/server"
-import Homepage from "./client/components/homepage/homepage.component"
+import { StaticRouter } from "react-router-dom";
+import Routes from "./client/Routes"
+import createStore from "./client/Store/createStore"
+import { Provider } from "react-redux";
 
 const app = express()
 
 app.use(express.static(`public`))
-app.get(`/`, (req, res) => {
-  const content = renderToString(<Homepage />)
+app.get(`*`, (req, res) => {
+  const store = createStore()
+  //logic for data into the sore
+
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
+  )
 
   const html = `
     <html>
-      <head></head>
+      <head>
+      </head>
       <body>
           <div id=root>${content}</div>
           <script src="bundle.js"></script> 
@@ -19,8 +33,9 @@ app.get(`/`, (req, res) => {
     </html>
   `;
 
+
   res.send(html)
 })
 app.listen(5000, () => {
-  console.log("server running")
+  process.stdout.write("server running")
 })
