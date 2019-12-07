10fdaddc721264e60518f335f759fc6d0600681d
mavroian <mavflorin@gmail.com>
2019-12-07 22:59:20 +0900

    created Creators reducers

---
+++ src/client/Reducers/creatorsReducer.js
@@ -0,0 +1,11 @@
+import { FETCH_CREATORS } from "../Actions/index";
+let initialState = { loaded: false, creators: [] }
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CREATORS:
+      return { ...state, loaded: true, creators: action.payload }
+    default:
+      return state;
+  }
+}
