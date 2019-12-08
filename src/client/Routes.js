e2e468af25b7203c1ee2ec261cd5553c7690975b
mavroian <mavflorin@gmail.com>
2019-12-08 19:35:06 +0900

    Added Navbar and Works Page

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom"
 import CreatorsList from "./components/creator-list/creatorList.component"
-import Profile from "./components/profile/profile"
+import Profile from "./components/profile/profile.component"
+import Works from "./components/works/creator-works.component"
+import Navbar from "./components/header/navbar.component"
+
 
 export default () => {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={CreatorsList} />
         <Route exact path="/:username" render={(props) => <Profile props={props} />} />
+        <Route exact path="/works/:workid" render={(props) => <Works props={props} />} />
       </Switch>
     </div>
   )
