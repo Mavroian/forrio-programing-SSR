8c0c976ae11b3ed1bf9281ef3d2220518fd866fc
mavroian <mavflorin@gmail.com>
2019-12-08 19:35:33 +0900

    Created Fetch WORKS actions

--- src/client/Actions/index.js
+++ src/client/Actions/index.js
@@ -15,3 +15,21 @@ export const fetchCreators = () => async dispatch => {
     payload: res.users
   });
 }
+
+//fetch creators works
+
+export const FETCH_WORKS = "fetch_works";
+export const fetchWorks = (id) => async dispatch => {
+
+  const res = await axios.get(`https://api.foriio.com/api/v1/works/${id}`).then(response => {
+    return response.data
+
+  }).catch((error) => {
+    console.error(error)
+  });
+
+  dispatch({
+    type: FETCH_WORKS,
+    payload: res.work
+  });
+}
